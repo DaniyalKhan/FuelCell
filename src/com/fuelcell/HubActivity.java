1094d3879891976c477b4f5c72edf42584932778
Ryan Nhieu <ryan.nhieu@gmail.com>
2014-09-28 22:36:15 -0400

    loading w/ example, remove/apply when everything is ready

--- src/com/fuelcell/HubActivity.java
+++ src/com/fuelcell/HubActivity.java
@@ -1,4 +1,5 @@
 package com.fuelcell;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,62 +19,67 @@ public class HubActivity extends Activity {
 	Button search;
 	Button findRoute;
 	Button favourite;
-	
+
 	public static List<Car> filtered;
 	ArrayList<Car> cars;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_hub);
-		
+
 		search = (Button) findViewById(R.id.search);
 		findRoute = (Button) findViewById(R.id.findRoute);
 		favourite = (Button) findViewById(R.id.favourite);
-		
+
 		ButtonSettings.pressSize(search, 15);
 		ButtonSettings.pressSize(findRoute, 15);
 		ButtonSettings.pressSize(favourite, 15);
-		
+
 		filtered = new ArrayList<Car>();
-		
+
 		setClick();
 	}
-	
-	private void setClick(){
-		search.setOnClickListener(new OnClickListener(){
+
+	private void setClick() {
+		search.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(HubActivity.this, SearchActivity.class);
+				Intent intent = new Intent(HubActivity.this,
+						SearchActivity.class);
 				startActivity(intent);
 			}
-			
+
 		});
-		findRoute.setOnClickListener(new OnClickListener(){
+		findRoute.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(HubActivity.this, DirectionsActivity.class);
+				Intent intent = new Intent(HubActivity.this,
+						DirectionsActivity.class);
 				startActivity(intent);
 			}
-			
+
 		});
-		favourite.setOnClickListener(new OnClickListener(){
+		favourite.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				startStatsActivity(Car.getSavedCars(HubActivity.this),"Saved",
-						"You do not currently have any car profiles saved.", Car.getSavedCars(HubActivity.this).size() > 0);
-//				Intent intent = new Intent(HubActivity.this, StatsActivity.class);
-//				startActivity(intent);
+				startStatsActivity(Car.getSavedCars(HubActivity.this), "Saved",
+						"You do not currently have any car profiles saved.",
+						Car.getSavedCars(HubActivity.this).size() > 0);
+				// Intent intent = new Intent(HubActivity.this,
+				// StatsActivity.class);
+				// startActivity(intent);
 			}
-			
+
 		});
 	}
-	
-	private void startStatsActivity(List<Car> cars,String title,String hint, boolean showClear) {
+
+	private void startStatsActivity(List<Car> cars, String title, String hint,
+			boolean showClear) {
 		Intent intent = new Intent(HubActivity.this, StatsActivity.class);
 		filtered = cars;
 		intent.putExtra("title", title);
@@ -82,15 +88,14 @@ public class HubActivity extends Activity {
 		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		startActivity(intent);
 	}
-	
-	private ArrayList<Car> filterList()
-	{
+
+	private ArrayList<Car> filterList() {
 		ArrayList<Car> filtered = new ArrayList<Car>();
-			for (int i = 0 ; i < cars.size(); i++) {
-				filtered.add(cars.get(i));
-				
-			}
-		
+		for (int i = 0; i < cars.size(); i++) {
+			filtered.add(cars.get(i));
+
+		}
+
 		return filtered;
 	}
 }
